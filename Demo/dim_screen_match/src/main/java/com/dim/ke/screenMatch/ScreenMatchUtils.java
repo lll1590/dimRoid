be139a651321386f5593d371c070681d37dfdce7
alsan_lin <454092825@qq.com>
2020-04-27 17:20:22 +0800

    限定符适配框架

---
+++ Demo/dim_screen_match/src/main/java/com/dim/ke/screenMatch/ScreenMatchUtils.java
@@ -0,0 +1,130 @@
+package com.dim.ke.screenMatch;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
+
+public class ScreenMatchUtils {
+
+    public static void printScreenParams(Activity activity){
+        Log.d("ScreenMatchUtils", "Mobile ScreenParams:\n" + getScreenParams(activity));
+    }
+
+    /**
+     * 获取屏幕相关参数
+     */
+    public static String getScreenParams(Activity context) {
+        DisplayMetrics dm = new DisplayMetrics();
+        context.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int heightPixels = getScreenHeight(context);
+        int widthPixels = getScreenWidth(context);
+        float xdpi = dm.xdpi;
+        float ydpi = dm.ydpi;
+        int densityDpi = dm.densityDpi;
+        float density = dm.density;
+        float scaledDensity = dm.scaledDensity;
+        float heightDP = heightPixels / density;
+        float widthDP = widthPixels / density;
+        float smallestWidthDP;
+        if (widthDP < heightDP) {
+            smallestWidthDP = widthDP;
+        } else {
+            smallestWidthDP = heightDP;
+        }
+        String str = "=== screen params ===";
+        str += "\nheightPixels: " + heightPixels + "px";
+        str += "\nwidthPixels: " + widthPixels + "px";
+        str += "\nxdpi: " + xdpi + "dpi";
+        str += "\nydpi: " + ydpi + "dpi";
+        str += "\ndensityDpi: " + densityDpi + "dpi";
+        str += "\ndensity: " + density;
+        str += "\nscaledDensity: " + scaledDensity;
+        str += "\nheightDP: " + heightDP + "dp";
+        str += "\nwidthDP: " + widthDP + "dp";
+        str += "\nsmallestWidthDP: " + smallestWidthDP + "dp";
+        return str;
+    }
+
+
+    /**
+     * dp值转换成px值
+     *
+     * @param dpValue dp值
+     * @return px值
+     */
+    public static int dp2px(Context context, final float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * px值转换成dp值
+     *
+     * @param pxValue px值
+     * @return dp值
+     */
+    public static int px2dp(Context context, final float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * sp值转换成px值
+     *
+     * @param spValue sp值
+     * @return px值
+     */
+    public static int sp2px(Context context, final float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * px值转换成sp值
+     *
+     * @param pxValue px值
+     * @return sp值
+     */
+    public static int px2sp(Context context, final float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 获取屏幕宽度
+     *
+     * @param context Context
+     * @return 屏幕宽度（px）
+     */
+    public static int getScreenWidth(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Point point = new Point();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            wm.getDefaultDisplay().getRealSize(point);
+        } else {
+            wm.getDefaultDisplay().getSize(point);
+        }
+        return point.x;
+    }
+
+    /**
+     * 获取屏幕高度
+     *
+     * @param context Context
+     * @return 屏幕高度（px）
+     */
+    public static int getScreenHeight(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Point point = new Point();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            wm.getDefaultDisplay().getRealSize(point);
+        } else {
+            wm.getDefaultDisplay().getSize(point);
+        }
+        return point.y;
+    }
+}
