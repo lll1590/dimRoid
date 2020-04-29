f6f41c8109e5a1ec8e0060620e5eb421062a7c86
alsan_lin <454092825@qq.com>
2020-04-29 09:42:35 +0800

    ConstraintLayout使用详解

---
+++ Demo/sample/src/main/java/com/dim/ke/sample/main/ConstraintLayoutDemoActivity.java
@@ -0,0 +1,16 @@
+package com.dim.ke.sample.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dim.ke.sample.R;
+
+public class ConstraintLayoutDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout_demo);
+    }
+}
